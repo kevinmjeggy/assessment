1637aa129a8350219bd9831c3e4a3f41b6e5ee8a
Kevin <kevinjeggy>
2023-02-19 23:54:21 +0530

    Initial Commit

---
+++ Kevin_Code/Kevin_CodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Kevin_CodeApp.swift
+//  Kevin_Code
+//
+//  Created by Kiran K Sibi on 19/02/23.
+//
+
+import SwiftUI
+
+@main
+struct Kevin_CodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
